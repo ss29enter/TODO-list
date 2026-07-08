9b2bc315e3e0e7c2f872b5d288f83af4bc51d68a
kss <curiosizee@gmail.com>
2026-07-08 11:33:34 +0300

    ADD NEW FEAT
    - обработка ошибок
    - вынос пользовательского ввода в функцию
    - выход из действия
    - улучшена структура кода

--- TODO/main.py
+++ TODO/main.py
@@ -6,20 +6,20 @@ def main():
     tasks = load_tasks()
     show_tasks(tasks)
     show_actions()
-    user = input('≫ ').lower()
+    user = user_input()
     while user != '4':
         if user == '1':
             add_task(tasks)
             clear_screen(tasks)
-            user = input('≫ ').lower()
+            user = user_input()
         if user == '2':
             mark_completed(tasks)
             clear_screen(tasks)
-            user = input('≫ ').lower()
+            user = user_input()
         if user == '3':
             tasks = remove_task(tasks)
             clear_screen(tasks)
-            user = input('≫ ').lower()
+            user = user_input()
     save_tasks(tasks)
 
 # запуск программы
--- TODO/task_manager.py
+++ TODO/task_manager.py
@@ -1,6 +1,18 @@
 from textwrap import wrap
 from os import system
 
+### интерфейс
+
+# меню
+def show_actions():
+    print_separator('menu')
+    print('1. Add a task')
+    print('2. Mark сompleted')
+    print('3. Remove a task')
+    print('4. Save & Exit')
+    print('X. Undo action\n')
+    print_separator('act')
+
 # вывести разделители 
 def print_separator(cmd):
     if cmd == 'title':
@@ -8,14 +20,36 @@ def print_separator(cmd):
     if cmd == 'menu':
         print(f'{'='*22} MENU {'='*22}\n')
     if cmd == 'sep':
-        print(f'{'='*25}{'='*25}\n')
+        print(f'{'='*50}\n')
     if cmd == 'act':
         print('='*50)
         print('\t\tSelect an action\n')
 
+# очищает экран перед выводом
+def clear_screen(tasks): 
+    system('cls')
+    show_tasks(tasks)
+    show_actions()
+
+# ввод пользователя
+def user_input():
+    user = input('≫ ').lower()
+    if user not in ['1','2','3','4']:
+        print('Неверный номер действия.')
+        return user_input()
+    return user
+
+# выход из действия
+def exit_from_action(user):
+    return user.lower() in ['x','exit']
+
+### действия
+
 # добавить задачу + присвоить ей номер
 def add_task(tasks):
     task = input('Task: ')
+    if exit_from_action(task):
+        return
     if task=='':
         return print('Вы ввели пустую строку.'), add_task(tasks)                             
     if tasks:
@@ -25,6 +59,7 @@ def add_task(tasks):
         'task': task,
         'done': False
     }
+
 # показать текущие задачи   
 def show_tasks(tasks):
     print_separator('title')
@@ -33,10 +68,21 @@ def show_tasks(tasks):
     for number in tasks.keys():
         id = tasks[number] 
         task = f'{number}. ☒  {id['task']}' if id['done'] else f'{number}. ☐  {id['task']}' 
-        for line in wrap(task,width=45):
+        for line in wrap(task,width=50):
             print(line)
     print()
     count_done(tasks)
+
+# отметить задачу завершенной
+def mark_completed(tasks):
+    number = input('Enter the number: ')
+    if exit_from_action(number):
+        return
+    try: 
+        tasks[number]['done'] = True
+    except KeyError:
+        print('Неверный номер задачи.')
+        mark_completed(tasks)
 
 # подсчитать кол-во завершенных/оставшихся задач
 def count_done(tasks):
@@ -52,32 +98,22 @@ def count_done(tasks):
         print(f'Завершенных: {done}')
         print(f'Осталось: {undone}') 
 
-# показать действия
-def show_actions():
-    print_separator('menu')
-    print('1. Add a task')
-    print('2. Mark сompleted')
-    print('3. Remove a task')
-    print('4. Save & Exit\n')
-    print_separator('act')
-
-# отметить задачу завершенной
-def mark_completed(tasks):
-    number = input('Enter the number: ')
-    tasks[number]['done'] = True
-
 # удалить задачу из списка + перенумеровать задачи  
 def remove_task(tasks):
     number = input('Enter the number: ')
-    del tasks[number]
+    if exit_from_action(number):
+        return
+    try:
+        del tasks[number]
+    except KeyError:
+        print('Неверный номер задачи.')
+        remove_task(tasks)
     new_tasks = {}
     for id,task in enumerate(tasks.values(),start=1):
         new_tasks[str(id)] = task
     return new_tasks
     
-# очищает экран перед выводом
-def clear_screen(tasks):
-    system('cls')
-    show_tasks(tasks)
-    show_actions()
+
+
+
 
