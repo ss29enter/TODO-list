eed703145a84dbd7585bf5cf3252940818a005c8
kss <curiosizee@gmail.com>
2026-07-08 17:22:47 +0300

    ADD NEW FEATURES
    - добавлена группировка по приоритетам
    - можно очистить весь список задач

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ def main():
     tasks = load_tasks()
     show_tasks(tasks)
     show_actions()
-    user = user_input()
+    user = user_input('u')
     while user != '4':
         if user == '1':
             add_task(tasks)
             clear_screen(tasks)
-            user = user_input()
+            user = user_input('u')
         if user == '2':
             mark_completed(tasks)
             clear_screen(tasks)
-            user = user_input()
+            user = user_input('u')
         if user == '3':
             tasks = remove_task(tasks)
             clear_screen(tasks)
-            user = user_input()
+            user = user_input('u')
     save_tasks(tasks)
 
 # запуск программы
--- task_manager.py
+++ task_manager.py
@@ -8,9 +8,9 @@ def show_actions():
     print_separator('menu')
     print('1. Add a task')
     print('2. Mark сompleted')
-    print('3. Remove a task')
+    print('3. Remove a task (tasks)')
     print('4. Save & Exit')
-    print('X. Undo action\n')
+    print('X. Exit the action\n')
     print_separator('act')
 
 # вывести разделители 
@@ -31,12 +31,15 @@ def clear_screen(tasks):
     show_tasks(tasks)
     show_actions()
 
-# ввод пользователя
-def user_input():
-    user = input('≫ ').lower()
+# ввод пользователя + ввод уровня приоритета
+def user_input(opt):
+    if opt == 'u':
+        user = input('>> ').lower()
+    if opt == 'p':
+        user = input('Priority level [1-4]: ')
     if user not in ['1','2','3','4']:
-        print('Неверный номер действия.')
-        return user_input()
+        print('Invalid action number.')
+        return user_input(opt)
     return user
 
 # выход из действия
@@ -45,33 +48,47 @@ def exit_from_action(user):
 
 ### действия
 
-# добавить задачу + присвоить ей номер
+# добавить задачу + присвоить ей номер + приоритет
 def add_task(tasks):
     task = input('Task: ')
+    priority = user_input('p')
+    priority_range = ['Critical','High','Medium','Low'][int(priority)-1]
     if exit_from_action(task):
         return
     if task=='':
-        return print('Вы ввели пустую строку.'), add_task(tasks)                             
+        return print('You entered an empty string.'), add_task(tasks)                             
     if tasks:
         new_id = str( max(map(int,tasks.keys()) ) + 1)
     else: new_id = '1'
     tasks[new_id] = {
         'task': task,
-        'done': False
+        'done': False,
+        'priority': priority_range
     }
 
-# показать текущие задачи   
+# показать текущие задачи + рассортировать по приоритету
 def show_tasks(tasks):
     print_separator('title')
+    priority_order = [
+        'Critical',
+        'High',
+        'Medium',
+        'Low'
+    ]
     if tasks == {}:
-        print("\t\tYou don't have any\n\t\tcurrent tasks yet")
-    for number in tasks.keys():
-        id = tasks[number] 
-        task = f'{number}. ☒  {id['task']}' if id['done'] else f'{number}. ☐  {id['task']}' 
-        for line in wrap(task,width=50):
-            print(line)
-    print()
-    count_done(tasks)
+        return print("\t\tYou don't have any\n\t\tcurrent tasks yet\n")
+
+    for priority in priority_order:
+        print('! ' + priority.upper())
+        for number in tasks.keys():
+            id = tasks[number]
+            if id['priority'] == priority:
+                task = f'{number}) ☒  {id['task']}' if id['done'] else f'{number}) ☐  {id['task']}' 
+                for line in wrap(task,width=48):
+                    print(' '*2 + line.ljust(48,'.'))
+            else: continue
+        print()
+    count_completed(tasks)
 
 # отметить задачу завершенной
 def mark_completed(tasks):
@@ -81,39 +98,36 @@ def mark_completed(tasks):
     try: 
         tasks[number]['done'] = True
     except KeyError:
-        print('Неверный номер задачи.')
+        print('Invalid issue number.')
         mark_completed(tasks)
 
 # подсчитать кол-во завершенных/оставшихся задач
-def count_done(tasks):
+def count_completed(tasks):
     done = 0
     for id in tasks:
         done += tasks[id]['done']
-    undone = len(tasks) - done
+    not_done = len(tasks) - done
     if tasks == {}:
          pass
     elif done == len(tasks):
-        print('Цель достигнута!')
+        print('All tasks are completed!')
     else:
-        print(f'Завершенных: {done}')
-        print(f'Осталось: {undone}') 
+        print(f'Done: {done}')
+        print(f'Remained: {not_done}') 
 
 # удалить задачу из списка + перенумеровать задачи  
 def remove_task(tasks):
-    number = input('Enter the number: ')
+    number = input('Enter the number or "ALL" to clear list: ')
+    if number == 'ALL':
+        return {}
     if exit_from_action(number):
         return
     try:
         del tasks[number]
     except KeyError:
-        print('Неверный номер задачи.')
+        print('Invalid issue number.')
         remove_task(tasks)
     new_tasks = {}
     for id,task in enumerate(tasks.values(),start=1):
         new_tasks[str(id)] = task
     return new_tasks
-    
-
-
-
-
